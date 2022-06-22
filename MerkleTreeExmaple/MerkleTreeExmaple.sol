901ab28b365b0e14a85af20d54ad8cda98675cb7
Pavan Ananth Sharma <86551444+PavanAnanthSharma@users.noreply.github.com>
2022-06-22 13:20:45 +0530

    Update MerkleTreeExmaple.sol

--- MerkleTreeExmaple/MerkleTreeExmaple.sol
+++ MerkleTreeExmaple/MerkleTreeExmaple.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.3;
 contract MerkleProof { 
     function verify( 
         bytes32[] memory proof, 
-        bytes32 root,
+        bytes32 root, 
         bytes32 leaf, 
         uint index
     ) public pure returns (bool) {
