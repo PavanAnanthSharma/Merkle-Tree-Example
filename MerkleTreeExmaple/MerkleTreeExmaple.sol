a6acd21b0b49e004fb07d04fa48c6de4a2392009
Pavan Ananth Sharma <86551444+PavanAnanthSharma@users.noreply.github.com>
2022-09-12 10:26:13 +0530

    Update MerkleTreeExmaple.sol

--- MerkleTreeExmaple/MerkleTreeExmaple.sol
+++ MerkleTreeExmaple/MerkleTreeExmaple.sol
@@ -5,7 +5,7 @@ contract MerkleProof {
     function verify( 
         bytes32[] memory proof, 
         bytes32 root, 
-        bytes32 leaf, 
+        bytes32 leaf,  
         uint index  
     ) public pure returns (bool) {  
         bytes32 hash = leaf;  
